b746accc59f08fbc397a86b7517bd4f3b3d36833
Muhammad Husnain <muhammadhasnain3031@gmail.com>
2026-09-19 18:59:09 +0500

    Time: 44 ms (18.27%) | Memory: 68 MB (45.09%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let map = {};
+    for(let str of strs){
+        const sortedStr = str.split('').sort().join('');
+        if(!map[sortedStr]){
+            map[sortedStr] = [];
+        }
+                    map[sortedStr].push(str)
+
+    }
+                return Object.values(map); 
+
+};
