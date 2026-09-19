63228b2a293ba6ae489ad551cfcd741e40088c54
Muhammad Husnain <muhammadhasnain3031@gmail.com>
2026-09-19 18:58:09 +0500

    Time: 2 ms (70.39%) | Memory: 55.7 MB (27.14%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+   const noteBook = new Map();
+   for (let i=0;i<nums.length;i++){
+    const currentNumber = nums[i];
+    const requiredNumber = target - currentNumber;
+    if(noteBook.has(requiredNumber)){
+        return [noteBook.get(requiredNumber), i];
+
+    }
+    noteBook.set(currentNumber,i)
+   }
+
+    
+};
