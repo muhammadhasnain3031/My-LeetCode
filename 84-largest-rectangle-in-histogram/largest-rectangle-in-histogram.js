13520e8655396b9ad762982ec9f2474417e89b95
Muhammad Husnain <muhammadhasnain3031@gmail.com>
2026-09-21 14:14:13 +0500

    Time: 9 ms (96.58%) | Memory: 67.3 MB (81.59%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function(heights) {
+    let n = heights.length;
+    let maxArea = 0;
+    let stack = [];
+    
+    for (let i=0; i <=n; i++){
+        let currentHeight = (i===n)?0:heights[i];
+        while(stack.length>0 && heights[stack[stack.length-1]]>currentHeight){
+            let height = heights[stack.pop()];
+            let width = (stack.length===0)?i:i-stack[stack.length-1]-1;
+            maxArea = Math.max(maxArea, height * width);
+
+        }
+        stack.push(i);
+    }
+    return maxArea;
+    
+};
