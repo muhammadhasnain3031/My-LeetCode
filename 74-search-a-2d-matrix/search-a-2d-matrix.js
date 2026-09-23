5989931d76cdd549c9cba8237c02a28d0d2b3dc8
Muhammad Husnain <muhammadhasnain3031@gmail.com>
2026-09-23 15:44:07 +0500

    Time: 0 ms (100.00%) | Memory: 53.7 MB (55.16%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    if(!matrix ===0 || matrix.length ===0 || matrix[0].length===0){
+        return fasle;
+    }
+    let rows = matrix.length;
+    let cols = matrix[0].length;
+
+    let left = 0;
+    let right = (rows * cols) - 1;
+
+    while(left <= right){
+        let mid = Math.floor((left + right)/2);
+        let rowIdx = Math.floor(mid/cols);
+        let colIdx = mid % cols;
+        let midElement = matrix[rowIdx][colIdx];
+        if(midElement===target){
+            return true;
+        }
+        else if(midElement < target){
+            left = mid +1;
+        }
+        else{
+            right = mid -1;
+        }
+        
+    }
+    return false;
+    
+};
