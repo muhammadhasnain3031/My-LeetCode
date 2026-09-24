6e43760c62205fed65fde380a230de75b70a21a4
Muhammad Husnain <muhammadhasnain3031@gmail.com>
2026-09-24 07:05:22 +0500

    Time: 8 ms (76.12%) | Memory: 59.1 MB (25.00%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    let low = 1;
+    let high = Math.max(...piles);
+    
+    while(low < high){
+        let mid = Math.floor((low + high)/2);
+        let totalHours = 0;
+        for(let pile of piles){
+            totalHours += Math.ceil(pile/mid);
+            }
+            if(totalHours <= h){
+                high = mid;
+            }
+            else{
+                low = mid +1;
+            }
+    }
+    return low
+
+    
+};
